d3598c485dfd26306abea4e78d8b5dbd079b9c61
imbireva <imbireva.daria@gmail.com>
2022-01-04 16:12:23 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
-}
+
+    (n < 0 ? n = -n : n = n);
+
+    const str = n.toString();
+
+    let i = 0;
+    let result = '';
+
+    while (i < str.length) {
+        result = str[i] + result;
+        i = i + 1;
+    }
+    return(result * 1);
+};
