c0a6869aec980b61f72d779c6c0c75dc0789d31e
Daria Imbireva <imbireva.daria@gmail.com>
2022-03-09 21:51:40 +0300

    update solution

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 module.exports = function reverse (n) {
+  n < 0 ? n = -n : n = n; // work with negative numbers
+  const str = String(n); // our number -> to string
+  let result = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    result = result + str[i];
+  }
+  return Number(result); // return number
+}
 
-    (n < 0 ? n = -n : n = n);
-
-    const str = n.toString();
-
-    let i = 0;
-    let result = '';
-
-    while (i < str.length) {
-        result = str[i] + result;
-        i = i + 1;
-    }
-    return(result * 1);
-};
